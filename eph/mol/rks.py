59e427f89aacdef0c7f002c6a1a84feb1455b6e4
Junjie Yang <yangjunjie0320@gmail.com>
2024-07-06 20:44:39 -0700

    add rks

--- eph/mol/rks.py
+++ eph/mol/rks.py
@@ -18,41 +18,45 @@ from eph.mol import eph_fd, rhf
 from eph.mol.rhf import ElectronPhononCouplingBase
 from eph.mol.eph_fd import harmonic_analysis
 
-def _get_vxc_deriv1(hessobj, mo_coeff, mo_occ, max_memory):
-    mol = hessobj.mol
-    mf = hessobj.base
-    if hessobj.grids is not None:
-        grids = hessobj.grids
-    else:
-        grids = mf.grids
-    if grids.coords is None:
-        grids.build(with_non0tab=True)
-
-    nao, nmo = mo_coeff.shape
-    ni = mf._numint
-    xctype = ni._xc_type(mf.xc)
+def _get_vxc_deriv1(mo_energy=None, mo_coeff=None, mo_occ=None, scf_obj=None, max_memory=2000, verbose=None):
+    # information from mol object
+    mol = scf_obj.mol
+    natm = mol.natm
     aoslices = mol.aoslice_by_atom()
     shls_slice = (0, mol.nbas)
     ao_loc = mol.ao_loc_nr()
-    dm0 = mf.make_rdm1(mo_coeff, mo_occ)
 
-    vmat = numpy.zeros((mol.natm,3,nao,nao))
-    max_memory = max(2000, max_memory-vmat.size*8/1e6)
+    # grids information
+    grids = scf_obj.grids
+    if grids.coords is None:
+        grids.build(with_non0tab=True)
+
+    # information from scf object
+    nao, nmo = mo_coeff.shape
+    ni = scf_obj._numint
+    xc = scf_obj.xc
+    xctype = ni._xc_type(xc)
+    dm0 = scf_obj.make_rdm1(mo_coeff, mo_occ)
+
+    # allocate memory for vmat
+    vmat = numpy.zeros((natm, 3, nao, nao))
+    max_memory = max(2000, max_memory - vmat.size * 8 / 1e6)
+
     if xctype == 'LDA':
         ao_deriv = 1
-        for ao, mask, weight, coords \
-                in ni.block_loop(mol, grids, nao, ao_deriv, max_memory):
+        block_loop = ni.block_loop(mol, grids, nao, ao_deriv, max_memory)
+        for ao, mask, weight, coords in block_loop:
             rho = ni.eval_rho2(mol, ao[0], mo_coeff, mo_occ, mask, xctype)
-            vxc, fxc = ni.eval_xc_eff(mf.xc, rho, 2, xctype=xctype)[1:3]
+            vxc, fxc = ni.eval_xc_eff(xc, rho, 2, xctype=xctype)[1:3]
+
             wv = weight * vxc[0]
             aow = numint._scale_ao(ao[0], wv)
 
             ao_dm0 = numint._dot_ao_dm(mol, ao[0], dm0, mask, shls_slice, ao_loc)
-            wf = weight * fxc[0,0]
-            for ia in range(mol.natm):
-                p0, p1 = aoslices[ia][2:]
-# First order density = rho1 * 2.  *2 is not applied because + c.c. in the end
-                rho1 = numpy.einsum('xpi,pi->xp', ao[1:,:,p0:p1], ao_dm0[:,p0:p1])
+            wf = weight * fxc[0, 0]
+
+            for ia, (s0, s1, p0, p1) in enumerate(aoslices):
+                rho1 = numpy.einsum('xpi,pi->xp', ao[1:, :, p0:p1], ao_dm0[:, p0:p1])
                 wv = wf * rho1
                 aow = [numint._scale_ao(ao[0], wv[i]) for i in range(3)]
                 rks_grad._d1_dot_(vmat[ia], mol, aow, ao[0], mask, ao_loc, True)
@@ -60,54 +64,29 @@ def _get_vxc_deriv1(hessobj, mo_coeff, mo_occ, max_memory):
 
     elif xctype == 'GGA':
         ao_deriv = 2
-        for ao, mask, weight, coords \
-                in ni.block_loop(mol, grids, nao, ao_deriv, max_memory):
+        block_loop = ni.block_loop(mol, grids, nao, ao_deriv, max_memory)
+        for ao, mask, weight, coords in block_loop:
             rho = ni.eval_rho2(mol, ao[:4], mo_coeff, mo_occ, mask, xctype)
-            vxc, fxc = ni.eval_xc_eff(mf.xc, rho, 2, xctype=xctype)[1:3]
+            vxc, fxc = ni.eval_xc_eff(xc, rho, 2, xctype=xctype)[1:3]
+
             wv = weight * vxc
-            wv[0] *= .5
-            ao_dm0 = [numint._dot_ao_dm(mol, ao[i], dm0, mask, shls_slice, ao_loc)
-                      for i in range(4)]
+            wv[0] *= 0.5
+
+            ao_dm0 = [numint._dot_ao_dm(mol, ao[i], dm0, mask, shls_slice, ao_loc) for i in range(4)]
             wf = weight * fxc
-            for ia in range(mol.natm):
+
+            for ia, (s0, s1, p0, p1) in enumerate(aoslices):
                 dR_rho1 = _make_dR_rho1(ao, ao_dm0, ia, aoslices, xctype)
                 wv = numpy.einsum('xyg,sxg->syg', wf, dR_rho1)
-                wv[:,0] *= .5
+                wv[:, 0] *= 0.5
                 aow = [numint._scale_ao(ao[:4], wv[i,:4]) for i in range(3)]
                 rks_grad._d1_dot_(vmat[ia], mol, aow, ao[0], mask, ao_loc, True)
             ao_dm0 = aow = None
 
     elif xctype == 'MGGA':
-        _check_mgga_grids(grids)
-        ao_deriv = 2
-        for ao, mask, weight, coords \
-                in ni.block_loop(mol, grids, nao, ao_deriv, max_memory):
-            rho = ni.eval_rho2(mol, ao[:10], mo_coeff, mo_occ, mask, xctype)
-            vxc, fxc = ni.eval_xc_eff(mf.xc, rho, 2, xctype=xctype)[1:3]
-            wv = weight * vxc
-            wv[0] *= .5
-            wv[4] *= .5  # for the factor 1/2 in tau
+        raise NotImplementedError('meta-GGA')
 
-            ao_dm0 = [numint._dot_ao_dm(mol, ao[i], dm0, mask, shls_slice, ao_loc) for i in range(4)]
-            wf = weight * fxc
-            for ia in range(mol.natm):
-                dR_rho1 = _make_dR_rho1(ao, ao_dm0, ia, aoslices, xctype)
-                wv = numpy.einsum('xyg,sxg->syg', wf, dR_rho1)
-                wv[:,0] *= .5
-                wv[:,4] *= .25
-                aow = [numint._scale_ao(ao[:4], wv[i,:4]) for i in range(3)]
-                rks_grad._d1_dot_(vmat[ia], mol, aow, ao[0], mask, ao_loc, True)
-
-                for j in range(1, 4):
-                    aow = [numint._scale_ao(ao[j], wv[i,4]) for i in range(3)]
-                    rks_grad._d1_dot_(vmat[ia], mol, aow, ao[j], mask, ao_loc, True)
-            ao_dm0 = aow = None
-
-    for ia in range(mol.natm):
-        p0, p1 = aoslices[ia][2:]
-        vmat[ia] = -vmat[ia] - vmat[ia].transpose(0,2,1)
-
-    return vmat
+    return -(vmat + vmat.transpose(0, 1, 3, 2))
 
 def gen_veff_deriv(mo_occ=None, mo_coeff=None, scf_obj=None, mo1=None, h1ao=None, verbose=None):
     log = logger.new_logger(None, verbose)
@@ -130,6 +109,7 @@ def gen_veff_deriv(mo_occ=None, mo_coeff=None, scf_obj=None, mo1=None, h1ao=None
     is_hybrid = ni.libxc.is_hybrid_xc(xc)
 
     vresp = scf_obj.gen_response(mo_coeff, mo_occ, hermi=1)
+    dvxc = _get_vxc_deriv1(mo_coeff, mo_occ, 2000)
 
     def load(ia):
         assert h1ao is not None
@@ -187,8 +167,6 @@ def gen_veff_deriv(mo_occ=None, mo_coeff=None, scf_obj=None, mo1=None, h1ao=None
             veff = vj1
 
         return t1, veff
-    
-    dvxc = _get_vxc_deriv1(scf_obj.Hessian(), mo_coeff, mo_occ, 2000)
 
     def func(ia):
         t1, vjk1 = load(ia)
@@ -225,10 +203,8 @@ if __name__ == '__main__':
     mf.kernel()
 
     grad = mf.nuc_grad_method().kernel()
-    print(grad)
     assert numpy.allclose(grad, 0.0, atol=1e-3)
     hess = mf.Hessian().kernel()
-    assert 1 == 2
 
     func = gen_veff_deriv(mo_occ=mf.mo_occ, mo_coeff=mf.mo_coeff, scf_obj=mf)
 
