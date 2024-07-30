1b7e0dbf6abb825d8eef8a1d291234440b853cfa
Junjie Yang <yangjunjie0320@gmail.com>
2024-07-29 22:29:08 -0700

    add pbc fd

--- examples/01-fd-with-mpi.py
+++ examples/01-fd-with-mpi.py
@@ -1,34 +1,41 @@
 import os, sys, pyscf
-from pyscf import gto, scf, lib
-from eph.mol.eph_fd import ElectronPhononCoupling
+from pyscf import lib
+from pyscf.pbc import gto, scf
 
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-mol = gto.M()
-mol.atom = '''
-O       0.0000000000     0.0000000000     0.1146878262
-H      -0.7540663886    -0.0000000000    -0.4587203947
-H       0.7540663886    -0.0000000000    -0.4587203947
+from pyscf.pbc import gto, scf
+cell = gto.Cell()
+cell.atom='''
+C 0.000000000000   0.000000000000   0.000000000000
+C 1.685068664391   1.685068664391   1.685068664391
 '''
-mol.basis = '631g*'
-mol.verbose = 0
-mol.symmetry = False
-mol.cart = True
-mol.unit = "AA"
-mol.build()
+cell.basis = 'gth-szv'
+cell.pseudo = 'gth-pade'
+cell.a = '''
+0.000000000, 3.370137329, 3.370137329
+3.370137329, 0.000000000, 3.370137329
+3.370137329, 3.370137329, 0.000000000'''
+cell.unit = 'B'
+cell.verbose = 0
+cell.mesh = [10, 10, 10]
+cell.build()
 
-mf = scf.RHF(mol)
-mf.conv_tol = 1e-12
-mf.conv_tol_grad = 1e-12
-mf.max_cycle = 1000
+mf = scf.RKS(cell)
+mf.xc = "PBE0"
+mf.init_guess = 'atom'
+mf.verbose = 0
+mf.max_cycle = 100
+mf.conv_tol = 1e-10
+mf.conv_tol_grad = 1e-8
 mf.kernel()
 
-import eph.mol.eph_fd
-fd_without_mpi = eph.mol.eph_fd._fd
-finalize_without_mpi = eph.mol.eph_fd.ElectronPhononCoupling._finalize
+import eph.pbc.eph_fd
+fd_without_mpi = eph.pbc.eph_fd._fd
+finalize_without_mpi = eph.pbc.eph_fd.ElectronPhononCoupling._finalize
 
 def _fd_with_mpi(**kwargs):
     from mpi4py import MPI
@@ -43,17 +50,17 @@ def _fd_with_mpi(**kwargs):
     
         scf_obj = kwargs['scf_obj']
         scf_obj.verbose = 4
-        scf_obj.mol.stdout = open(f, "w")
+        scf_obj.stdout = open(f, "w")
 
         res = fd_without_mpi(**kwargs)
         
-        scf_obj.mol.stdout.close()
-        scf_obj.mol.stdout = sys.stdout
+        scf_obj.stdout.close()
+        scf_obj.stdout = sys.stdout
         return (ix, res)
     else:
         return None
     
-eph.mol.eph_fd._fd = _fd_with_mpi
+eph.pbc.eph_fd._fd = _fd_with_mpi
 
 def _finalize_with_mpi(eph_obj, dv_ao):
     dv_ao = comm.gather(dv_ao, root=0)
@@ -65,9 +72,10 @@ def _finalize_with_mpi(eph_obj, dv_ao):
     dv_ao = comm.bcast(dv_ao, root=0)
     return dv_ao
 
-eph.mol.eph_fd.ElectronPhononCoupling._finalize = _finalize_with_mpi
+eph.pbc.eph_fd.ElectronPhononCoupling._finalize = _finalize_with_mpi
 
 # Test the finite difference against the analytic results
+from eph.pbc.eph_fd import ElectronPhononCoupling
 eph_obj = ElectronPhononCoupling(mf)
 eph_obj.verbose = 0
 
