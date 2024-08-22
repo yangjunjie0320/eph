9f18f3f1696e0f037a988725307847d933bcbf95
Junjie Yang <yangjunjie0320@gmail.com>
2024-08-22 15:32:32 -0700

    update

--- eph/mol/rhf.py
+++ eph/mol/rhf.py
@@ -33,7 +33,7 @@ def kernel(eph_obj, mo_energy=None, mo_coeff=None, mo_occ=None,
             mo_energy=mo_energy, mo_coeff=mo_coeff, 
             mo_occ=mo_occ, chkfile=chkfile,
             atmlst=atmlst, verbose=log
-            ) # do I really need to keep all of this?
+            )
     assert os.path.exists(chkfile), '%s not found' % chkfile
 
     from pyscf import dft
@@ -58,46 +58,25 @@ def kernel(eph_obj, mo_energy=None, mo_coeff=None, mo_occ=None,
 
     assert omega == 0.0
 
-    hcore_deriv = eph_obj.gen_hcore_deriv()
-    vnuc_deriv  = eph_obj.gen_vnuc_deriv(mol_obj)
-    veff_resp   = scf_obj.gen_response(mo_coeff, mo_occ, hermi=1)
-    ipovlp = eph_obj.get_ipovlp()
+    vnuc_deriv = eph_obj.gen_vnuc_deriv(mol_obj)
+    vresp = scf_obj.gen_response(mo_coeff, mo_occ, hermi=1)
     
     dv_ao = [] # numpy.zeros((len(atmlst), 3, nao, nao))
     for i0, ia in enumerate(atmlst):
-        # t1 = lib.chkfile.load(chkfile, 'scf_mo1/%d' % ia)
-        # t1 = t1.reshape(-1, nao, nmo)
+        t1 = lib.chkfile.load(chkfile, 'scf_mo1/%d' % ia)
+        t1 = t1.reshape(-1, nao, nmo)
 
-        # vjk1 = lib.chkfile.load(chkfile, 'scf_j1ao/%d' % ia)
-        # if is_hybrid:
-        #     vjk1 -= 0.5 * lib.chkfile.load(chkfile, 'scf_k1ao/%d' % ia)
-        # v1 = vjk1 + vjk1.transpose(0, 2, 1)
+        vjk1 = lib.chkfile.load(chkfile, 'scf_j1ao/%d' % ia)
+        if is_hybrid:
+            vk1 = lib.chkfile.load(chkfile, 'scf_k1ao/%d' % ia)
+            vjk1 -= 0.5 * vk1 * hyb
 
-        # dm1 = 
-        h1 = hcore_deriv(ia)
-        s1 = None # for cphf equation
-        f1 = None # for cphf equation
+        v1 = vjk1 + vjk1.transpose(0, 2, 1)
+        dm1 = numpy.dot(t1, mo_coeff.T)
+        dm1 = numpy.dot(dm1, dm1.T) * 2.0
 
-        # solve cphf equation with s1 and f1
-
-        vjk1 = None
-        dm1 = None
-        v1 = vjk1 + vresp(dm1)
-
-        f1, h1
-
-        vjk1, t1 = eph_obj.solve_mo1(
-            mo_energy=mo_energy, mo_coeff=mo_coeff,
-            mo_occ=mo_occ, chkfile=chkfile,
-            atmlst=[ia], verbose=log
-        ) 
-        # vjk1 is the direct contribution from 2e part
-        # t1 is the response of MO/density
-
-        dm1 = eph_obj.get_dm1(t1, mo_occ)
-
-        v1 = vjk1 + vresp(dm1)
-        v1 += h1
+        v1 = v1 + vnuc_deriv(ia)
+        v1 = v1 + vresp(dm1)
         dv_ao.append(v1)
 
     dv_ao = numpy.array(dv_ao).reshape(len(atmlst), -1, 3, nao, nao)
@@ -132,7 +111,7 @@ class ElectronPhononCouplingBase(eph.mol.eph_fd.ElectronPhononCouplingBase):
                              scf_obj=None, mo1=None, h1ao=None, verbose=None):
         raise NotImplementedError
 
-    def solve_mo1(self, ia=0, mo_energy=None, mo_coeff=None, mo_occ=None, 
+    def solve_mo1(self, mo_energy=None, mo_coeff=None, mo_occ=None, 
                         chkfile=None, atmlst=None, verbose=logger.DEBUG):
         if mo_energy is None: mo_energy = self.base.mo_energy
         if mo_coeff is None:  mo_coeff = self.base.mo_coeff
@@ -141,7 +120,23 @@ class ElectronPhononCouplingBase(eph.mol.eph_fd.ElectronPhononCouplingBase):
         if chkfile is None:
             chkfile = self.chkfile
 
+        from mo1 import get_h1ao, solve_mo1
+        get_h1ao(
+            self.base, mo_energy=mo_energy, 
+            mo_coeff=mo_coeff, mo_occ=mo_occ,
+            chkfile=chkfile, atmlst=atmlst, 
+            verbose=verbose
+        )
 
+        assert os.path.exists(chkfile), '%s not found' % chkfile
+        solve_mo1(
+            self.base, mo_energy=mo_energy, 
+            mo_coeff=mo_coeff, mo_occ=mo_occ,
+            chkfile=chkfile, conv_tol=self.conv_tol_cphf,
+            atmlst=atmlst, max_cycle=self.max_cycle_cphf,
+            verbose=verbose
+        )
+        return chkfile
 
     def kernel(self, mo_energy=None, mo_coeff=None, mo_occ=None, atmlst=None):
         if mo_energy is None: mo_energy = self.base.mo_energy
@@ -163,11 +158,6 @@ class ElectronPhononCoupling(ElectronPhononCouplingBase):
     def __init__(self, method):
         assert isinstance(method, scf.hf.RHF)
         ElectronPhononCouplingBase.__init__(self, method)
-
-    def gen_hcore_deriv(self):
-        grad_obj = self.base.nuc_grad_method()
-        hcore_deriv = grad_obj.hcore_generator()
-        return hcore_deriv
 
     def gen_veff_deriv(self, mo_energy=None, mo_coeff=None, mo_occ=None, 
                              scf_obj=None, mo1=None, h1ao=None, verbose=None):
